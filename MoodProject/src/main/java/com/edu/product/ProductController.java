e00fe511bd2c955d08a0cb666998a86f4b69fb50
(kngom) <(kngom1998@gmail.com)>
2023-03-23 16:50:18 +0900

    2차컨트롤러

--- MoodProject/src/main/java/com/edu/product/ProductController.java
+++ MoodProject/src/main/java/com/edu/product/ProductController.java
@@ -14,7 +14,7 @@ public class ProductController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
-	//카테고리에 따른(상품 타입) 상품 리스트 페이지 이동 + 정렬 방식에 따른 상품 리스트 데이터 함께 넘기기
+	//카테고리에 따른(상품 타입) 상품 리스트 페이지 이동 + 정렬 방식에 따른 상품 리스트 데이터 함께 넘기기기기
 	@RequestMapping(value="productList", method=RequestMethod.GET)
 	public ModelAndView productList()throws Exception{		
 		

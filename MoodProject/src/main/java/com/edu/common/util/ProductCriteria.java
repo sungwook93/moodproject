cae2baeca969313ab6842187fbac9be945a16374
woogi <qkrtjddnr963@naver.com>
2023-03-24 10:59:00 +0900

    ProductCriteria 추가

---
+++ MoodProject/src/main/java/com/edu/common/util/ProductCriteria.java
@@ -0,0 +1,42 @@
+package com.edu.common.util;
+
+public class ProductCriteria extends Criteria {
+	
+	private String product_type;	//상품 타입
+	private String product_color;  //상품 색상
+	private String searchType; //검색 조건
+	private String keyword; // 검색 키워드
+	
+	public String getProduct_type() {
+		return product_type;
+	}
+	public void setProduct_type(String product_type) {
+		this.product_type = product_type;
+	}
+	public String getProduct_color() {
+		return product_color;
+	}
+	public void setProduct_color(String product_color) {
+		this.product_color = product_color;
+	}
+	public String getSearchType() {
+		return searchType;
+	}
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductCriteria [product_type=" + product_type + ", product_color=" + product_color + ", searchType="
+				+ searchType + ", keyword=" + keyword + "]";
+	}
+	
+	
+}

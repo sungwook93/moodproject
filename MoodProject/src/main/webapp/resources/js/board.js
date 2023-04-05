ed7e69b54231a25fcc5fb7e939fe03d2baf77436
jinwoo kuk <jinwoo4301@naver.com>
2023-04-05 20:18:46 +0900

    board.js 검색 기능 추가

--- MoodProject/src/main/webapp/resources/js/board.js
+++ MoodProject/src/main/webapp/resources/js/board.js
@@ -353,4 +353,23 @@ function fn_updateOpen(reply_bno) {
 	
  }  
 
- 
+ // 검색 
+
+$(document).ready(function() {
+			
+	var formObj = $("#formList");
+	
+	// 검색 버튼을 눌렀을 경우
+	$("#searchBtn").click(function() {
+		var typeStr		= $("#searchType").find(":selected").val();
+		var keywordStr	= $("#searchKeyword").val();
+		alert(typeStr + ":" + keywordStr);
+		
+		// 서버로 전송하기 전에, name 속성에 값을 넣어준다.
+		formObj.find("[name='searchType']").val(typeStr);
+		formObj.find("[name='keyword']").val(keywordStr);
+		formObj.find("[name='page']").val("1");
+		formObj.submit();
+	});
+	
+});

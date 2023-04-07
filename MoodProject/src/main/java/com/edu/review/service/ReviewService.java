f409b6ece976e7121916a63ca3b3304a0b68e9f3
jinwoo kuk <jinwoo4301@naver.com>
2023-04-07 12:56:58 +0900

    리뷰 파일

---
+++ MoodProject/src/main/java/com/edu/review/service/ReviewService.java
@@ -0,0 +1,24 @@
+package com.edu.review.service;
+
+import java.util.List;
+
+import com.edu.common.util.SearchCriteria;
+import com.edu.order.dto.CartDTO;
+import com.edu.product.dto.ProductDTO;
+import com.edu.review.dto.ReviewDTO;
+
+public interface ReviewService {
+	
+	// 리뷰 목록 보여주기
+	public List<ReviewDTO> reviewList(SearchCriteria sCri) throws Exception;
+	
+	// 전체 페이지 가져오기
+	public int reviewListTotalCount(SearchCriteria sCri) throws Exception;
+	
+	// 리뷰 등록하기
+	public int reviewRegister(ReviewDTO reviewDTO) throws Exception;
+	
+	// productList 가져오기
+	public List<ProductDTO> productList(ProductDTO productDTO) throws Exception;
+	
+}
---
+++ MoodProject/src/main/java/com/edu/review/service/ReviewServiceImpl.java
@@ -0,0 +1,65 @@
+package com.edu.review.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.edu.common.util.SearchCriteria;
+import com.edu.product.dto.ProductDTO;
+import com.edu.review.dao.ReviewDAO;
+import com.edu.review.dto.ReviewDTO;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+	
+	@Inject
+	private ReviewDAO reviewDAO;
+	
+	
+	// 리뷰 목록 보여주기
+	@Override
+	public List<ReviewDTO> reviewList(SearchCriteria sCri) throws Exception {
+		
+		return reviewDAO.reviewList(sCri);
+	}
+	
+	// 전체 페이지 가져오기
+	@Override
+	public int reviewListTotalCount(SearchCriteria sCri) throws Exception {
+		
+		return reviewDAO.reviewListTotalCount(sCri);
+	}
+	
+	// 리뷰 등록하기
+	@Override
+	public int reviewRegister(ReviewDTO reviewDTO) throws Exception {
+		
+		System.out.println("ReviewServiceImpl 리뷰 등록하기");
+		
+		return reviewDAO.reviewRegister(reviewDTO);
+	}
+	
+	// productList 가져오기
+	@Override
+	public List<ProductDTO> productList(ProductDTO productDTO) throws Exception {
+		
+		return reviewDAO.productList(productDTO);
+	}
+
+	
+	
+
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+}

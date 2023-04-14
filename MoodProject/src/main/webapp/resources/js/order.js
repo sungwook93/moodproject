ee8d16dc4c692efe5f5f304ea95177ca4e1adb43
(kngom) <(kngom1998@gmail.com)>
2023-04-14 09:12:01 +0900

    23

--- MoodProject/src/main/webapp/resources/js/order.js
+++ MoodProject/src/main/webapp/resources/js/order.js
@@ -20,7 +20,7 @@ function fn_myorderpage() {
 	let userID = document.getElementById('userID').value;
 	let ordernameid= document.getElementById('ordernameid').value;
 	alert(userID);
-	location.href="/member/myPageForm.do?userID="+userID+"ordernameid;
+	location.href="/member/myPageForm.do?userID="+ordernameid;
 	
 	//처음 페이지들어갈시 상품관리메뉴만보이게
 	$("#membertitle").css("display", "none");
